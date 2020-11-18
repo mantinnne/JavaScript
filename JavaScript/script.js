1dc5df7c8b6a0e8ff6c0b38de5d284ab98d4528a
Mantinne <as.nanao@gmail.com>
2020-11-18 22:42:55 +0300

    JavaScript

---
+++ JavaScript/script.js
@@ -0,0 +1,29 @@
+"use sctrict";
+
+let number = 4.6;
+
+// console.log(-4/0); //invinity
+
+// console.log('string' * 9);
+
+const persone = "Alex";
+
+const bool = true / false;
+
+// console.log(something);
+
+let und;
+console.log(und);
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+    };
+
+console.log(obj.name);
+console.log(obj["name"]);
+
+let arr = ['plum.png', 'orange.jpng', 6, 'apple.bmp', {}, []];
+
+console.log(arr[1]);
