bf2e7c3f9fd4e1dd7bc2cfb9ad0d11dc7bf984d1
Mantinne <as.nanao@gmail.com>
2020-11-19 18:00:41 +0300

    lessen for/case/while

--- JavaScript/script5.js
+++ JavaScript/script5.js
@@ -10,15 +10,39 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмо?', ''),
-      b = prompt('На сколько оцение его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его', '');
 
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмо?', ''),
+        b = prompt('На сколько оцение его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
 
-
+    } else {
+        console.log('Error');
+        i--;
+    }
+}
 console.log(personalMovieDB);
 
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно много фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман!");
+} else {
+    console.log("Произошла ошибка");
+}
+
+
+// const a1 = prompt('Один из последних просмотренных фильмо?', ''),
+// b1 = prompt('На сколько оцение его?', '');
+// while ( a1.length < 50){
+//     const a1 = prompt('Один из последних просмотренных фильмо?', ''),
+//           b = prompt('На сколько оцение его?', '');
+// }
+
+
+// console.log(personalMovieDB);
---
+++ JavaScript/script6.js
@@ -0,0 +1,37 @@
+"use strict";
+
+if (4 == 5 ) {
+    console.log('ok!');
+} else {
+    console.log("not OK!");
+}
+
+// const num = 50;
+
+// if (num < 49) {
+//     console.log('Error');
+// } else if ( num > 100) {
+//     console.log('Много');
+// } else {
+//     console.log('Ok');
+// }
+
+// (num == 50) ? console.log('Ok') : console.log('Error'); // тернарный  оператор, работает по аналогии с if 
+
+const num = 48;
+
+switch (num) {
+    case 49: 
+        console.log('Incorrect');
+        break;
+    case 100:
+        console.log('Incorrect');
+        break;
+    case 50:
+        console.log('Ok');
+        break;
+    default:
+        console.log('не в этот раз');
+        break;
+
+}
